03d47459cb876e5cf7b99d023b189af1bf1fde53
Arpana Kumari <kashyaparpana203@gmail.com>
2022-12-18 18:33:23 +0530

    touchchange

--- Index.js
+++ Index.js
@@ -1,56 +1,136 @@
 const draggableElement1 = document.getElementById("draggable-element1");
 const draggableElement2 = document.getElementById("draggable-element2");
-const lists = document.querySelectorAll(".list");
+const dropZone = document.getElementById("dropzone");
+let lists = document.querySelectorAll(".list");
+const greet = document.querySelector(".greet");
+const ingredient_title = document.querySelector('.ingredient-title')
 
 draggableElement1.addEventListener("dragover", function (e) {
   e.preventDefault();
   setTimeout(() => {
-    document.getElementById("dropzone").classList.add("add1");
-  }, 0);
-  console.log("over");
+    dropZone.classList.add("add1");
+    dropZone.classList.remove("add2");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
+    greet.style.visibility = "hidden";
+  }, 1000);
+});
+draggableElement1.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  setTimeout(() => {
+    dropZone.classList.add("add1");
+    dropZone.classList.remove("add2");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
+    greet.style.visibility = "hidden";
+    document.getElementById("ul").style.visibility = "visible";
+    ingredient_title.style.visibility = "visible";
+  }, 500);
 });
 
-draggableElement2.addEventListener("dragenter", function () {
-  console.log("enter");
-});
+
+
 draggableElement2.addEventListener("dragover", function (e) {
   e.preventDefault();
-  document.getElementById("dropzone").classList.add("add2");
-  console.log("over");
+  setTimeout(() => {
+    dropZone.classList.add("add2");
+    dropZone.classList.remove("add1");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
+    greet.style.visibility = "hidden";
+  }, 1000);
+});
+draggableElement2.addEventListener("touchend", function (e) {
+  e.preventDefault();
+  setTimeout(() => {
+    dropZone.classList.add("add2");
+    dropZone.classList.remove("add1");
+    dropZone.classList.remove("pizza1");
+    dropZone.classList.remove("pizza2");
+    dropZone.classList.remove("pizza3");
+    greet.style.visibility = "hidden";
+    document.getElementById("ul").style.visibility = "visible";
+    ingredient_title.style.visibility = "visible";
+  }, 500);
 });
 
 lists.forEach((element, index) => {
   if (index === 0) {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
-      document.getElementById("dropzone").classList.add("pizza1");
-      console.log("over");
+      setTimeout(() => {
+        dropZone.classList.add("pizza1");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza3");
+        greet.style.visibility = "visible";
+      }, 1000);
     });
+
+    element.addEventListener("touchend", function (e) {
+      e.preventDefault();
+      setTimeout(() => {
+        dropZone.classList.add("pizza1");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza3");
+        greet.style.visibility = "visible";
+      }, 1000);
+    });
+    
+    
   } else if (index === 1) {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
-      document.getElementById("dropzone").classList.add("pizza2");
-      document.getElementById("dropzone").classList.remove("pizza1");
-      console.log("over");
+      setTimeout(() => {
+        dropZone.classList.add("pizza2");
+        dropZone.classList.remove("pizza1");
+        dropZone.classList.remove("pizza3");
+        greet.style.visibility = "visible";
+      }, 1000);
+    });
+
+    element.addEventListener("touchend", function (e) {
+      e.preventDefault();
+      setTimeout(() => {
+        dropZone.classList.add("pizza2");
+        dropZone.classList.remove("pizza1");
+        dropZone.classList.remove("pizza3");
+        greet.style.visibility = "visible";
+      }, 1000);
     });
   } else {
     element.addEventListener("dragover", function (e) {
       e.preventDefault();
-      document.getElementById("dropzone").classList.add("pizza3");
-      document.getElementById("dropzone").classList.remove("pizza2");
-      console.log("over");
+      setTimeout(() => {
+        dropZone.classList.add("pizza3");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza1");
+        greet.style.visibility = "visible";
+        
+      }, 1000);
+    });
+
+    element.addEventListener("touchend", function (e) {
+      e.preventDefault();
+      setTimeout(() => {
+        dropZone.classList.add("pizza3");
+        dropZone.classList.remove("pizza2");
+        dropZone.classList.remove("pizza1");
+        greet.style.visibility = "visible";
+      }, 1000);
     });
   }
 });
 
-const dropzone = document.getElementById("dropzone");
-dropzone.addEventListener("dragover", function (event) {
+
+dropZone.addEventListener("dragover", function (event) {
   event.preventDefault();
-  console.log("over");
 });
 
-dropzone.addEventListener("drop", function (event) {
+dropZone.addEventListener("drop", function (event) {
   event.preventDefault();
-
   document.getElementById("ul").style.visibility = "visible";
+  ingredient_title.style.visibility = "visible";
 });
